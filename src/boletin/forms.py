0c02b1fd5aeb4f7d30ee2e6c7e81af881457f9eb
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-18 14:16:30 -0300

    model form

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
+#model form
+from .models import Registrado
+
+class RegModelForm(forms.ModelForm):
+    class Meta:
+        model = Registrado
+        fields = ["nombre","email"]
+
 
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
-    email = forms.EmailField()
+    email = forms.EmailField() 
