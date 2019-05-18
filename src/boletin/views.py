9474b47a7e5f9221ac12e306221f5cb27735f814
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-18 16:48:29 -0300

    configurar envio de email

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import RegModelForm, ContactForm
@@ -45,12 +47,21 @@ def inicio(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        email = form.cleaned_data.get("email")
-        mensaje = form.cleaned_data.get("mensaje")
-        nombre = form.cleaned_data.get("nombre")
-        #print email,mensaje, nombre
+        form_email = form.cleaned_data.get("email")
+        form_mensaje = form.cleaned_data.get("mensaje")
+        form_nombre = form.cleaned_data.get("nombre")
+        assunto = 'Form  de contato'
+        email_from = settings.EMAIL_HOST_USER
+        email_to = [email_from, "otroemail@gmail.com"]
+        email_mensaje = "%s: %s enviado por %s" %(form_nombre,form_mensaje,form_email)
 
-        context = {
-            "form" : form,
-        }
+        send_mail(assunto,
+                email_mensaje,
+                email_from,
+                email_to,
+                fail_silently = False
+                )
+    context = {
+        "form" : form,
+    }
     return render(request, "forms.html", context)
