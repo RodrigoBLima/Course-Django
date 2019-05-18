9768a4f5d8fcfe55e13e4a2d96cd41e1733b1d04
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-18 20:44:47 -0300

    urls

--- src/pydjango/urls.py
+++ src/pydjango/urls.py
@@ -21,12 +21,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from boletin import views
+from .views import about
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.inicio, name='inicio'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', about, name='about'),
+
 
 ]
 
