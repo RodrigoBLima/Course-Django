dcee78bb372fad4e8d8104cfb99889e3197a9959
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-18 15:27:25 -0300

    model form in view

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm,RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -8,17 +8,35 @@ def inicio(request):
     titulo = "Hola"
     if request.user.is_authenticated():
         titulo = "Bem vindo %s" %(request.user)
-    form = RegForm(request.POST or None)
+    form = RegModelForm(request.POST or None)
+
+    context = {  
+                "titulo": titulo,
+                "el_form" : form,
+            }
 
     if form.is_valid(): 
-        form_data = form.cleaned_data
-        abc = form_data.get("email")
-        abc2 = form_data.get("nombre")
-        obj = Registrado.objects.create(email=abc, nombre=abc2)
+        instance = form.save(commit=False)
+        nombre = form.cleaned_data.get("nombre")
+        email = form.cleaned_data.get("email")
+        if not instance.nombre:
+            instance.nombre = "Pessoa"
+        instance.save() 
+
+        context = {
+            "titulo" : "Obrigado %s!" %(nombre)
+        }
+
+        if not nombre:
+           context = {
+            "titulo" : "Obrigado pessoa sem nome %s!" %(email) 
+            } 
+
+        #form_data = form.cleaned_data
+        #abc = form_data.get("email")
+        #abc2 = form_data.get("nombre")
+        #obj = Registrado.objects.create(email=abc, nombre=abc2)
 
 
-    context = {
-        "titulo": titulo,
-        "el_form" : form,
-    }
+   
     return render(request, "inicio.html", context)
